21a2d44d9ad082f09105e4912b8d224d3c42f481
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-07-21 15:04:32 +0200

    allow deploy uniswap v3 on aurora testnet

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # uniswap-aurora-deployer
+
+
+## Install
+
+```
+yarn 
+```
+
+## Deploy
+
+```
+npx hardhat deploy-uniswap --network testnet_aurora
+```
---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomiclabs/hardhat-waffle");
+require("uniswap-v3-deploy-plugin");
+require('dotenv').config();
+
+const AURORA_PRIVATE_KEY = process.env.AURORA_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    testnet_aurora: {
+      url: 'https://testnet.aurora.dev',
+      accounts: [`0x${AURORA_PRIVATE_KEY}`],
+      chainId: 1313161555,
+      gasPrice: 120 * 1000000000
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${AURORA_PRIVATE_KEY}`],
+      chainId: 3,
+      live: true,
+      gasPrice: 50000000000,
+      gasMultiplier: 2,
+    },
+  }
+};
